8c9f67dd360bde78c6d93c059873464f1fdcf4eb
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-03-17 23:48:39 +0700

    add ulti image and colors

---
+++ lib/util/image_asset.dart
@@ -0,0 +1,20 @@
+class Assets {
+  Assets._();
+
+  // icon
+  static const String binImg = 'assets/images/bin.png';
+  static const String earthImg = 'assets/images/earth.png';
+  static const String footerImg = 'assets/images/footer_waiting.png';
+  static const String flowerImg = 'assets/images/flower.png';
+  static const String unionImg = 'assets/images/union.png';
+  static const String correctImg = 'assets/images/correct.png';
+  static const String wrongImg = 'assets/images/wrong.png';
+  static const String bgImg = 'assets/images/bg.png';
+  static const String recycleImg = 'assets/images/recycle.png';
+  static const String nonrecycleImg = 'assets/images/nonrecycle.png';
+  static const String redFaceImg = 'assets/images/redFace.png';
+  static const String greenFaceImg = 'assets/images/greenFace.png';
+  static const String faceImg = 'assets/images/face-id.png';
+  static const String faceScan = 'assets/icons/face_scan.json';
+  static const String faceScaned = 'assets/images/scaned.png';
+}
